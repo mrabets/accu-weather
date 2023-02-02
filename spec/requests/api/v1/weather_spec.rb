8191665801510f7d31ea1dcb65a4aae2cf04606c
Karim Marabet <karim.marabet@productmadness.com>
2023-02-02 13:13:33 +0100

    Setup Swagger

---
+++ spec/requests/api/v1/weather_spec.rb
@@ -0,0 +1,38 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/weather', type: :request do
+
+  path '/api/v1/weather/current' do
+
+    get('current weather') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/weather/by_time' do
+
+    get('by_time weather') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
